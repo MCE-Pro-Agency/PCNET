be5008287212696ee5ec65d568115df6b23bfdb0
MCE Group-- Management Communication Event <mceproagency@gmail.com>
2026-04-23 10:41:33 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import WhatsAppButton from "@/components/WhatsAppButton";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import CGV from "./pages/CGV";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import QuiSuisJe from "./pages/QuiSuisJe";
@@ -20,6 +21,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/qui-suis-je" element={<QuiSuisJe />} />
+          <Route path="/cgv" element={<CGV />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <WhatsAppButton />
